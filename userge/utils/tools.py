df2e7e8b184ef57f31e17879b12481227b67d84f
Chandan Maharana <chandanmaharana144@gmail.com>
2021-02-06 22:12:59 +0530

    juss removed author name and url

--- userge/utils/tools.py
+++ userge/utils/tools.py
@@ -77,8 +77,6 @@ def post_to_telegraph(a_title: str, content: str) -> str:
     post_client.create_api_token(auth_name)
     post_page = post_client.post(
         title=a_title,
-        author=auth_name,
-        author_url="https://t.me/x_xtests",
         text=content,
     )
     return post_page["url"]
